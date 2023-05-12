29447da280b1972eef60eb6c0408e9f0aa45d71c
Saurabh Bhatt <saurabhbhatt1999@gmail.com>
2023-05-12 10:22:42 +0530

    search complete

--- src/App.js
+++ src/App.js
@@ -4,10 +4,30 @@ import { Data } from "./data";
 function App() {
   const [state, setstate] = useState(Data);
   const [team, setTeam] = useState(0);
+  const [search, setSearch] = useState("");
+
+  const filterData = (search, state) => {
+    const filterData = state.filter((state) =>
+      state.first_name.includes(search)
+    );
+    return filterData;
+  };
   return (
     <div className="App">
-      <input type="search" name="" id="" placeholder="Search here" />
-      <button>Submit</button>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search here"
+      />
+      <button
+        onClick={() => {
+          const data = filterData(search, state);
+          setSearch(data);
+        }}
+      >
+        Search
+      </button>
       <br />
       <h3>Filter</h3>
       <button
@@ -41,19 +61,17 @@ function App() {
       <hr />
       {state.map((item) => {
         return (
-          <>
-            <div className="main-card">
-              <div className="card">
-                <img src={item.avatar} alt="" />
-                <h2>
-                  {item.first_name} {item.last_name}
-                </h2>
-                <p className="add" onClick={() => setTeam(team + 1)}>
-                  Add to team
-                </p>
-              </div>
+          <div className="main-card" key={item.id}>
+            <div className="card">
+              <img src={item.avatar} alt="" />
+              <h2>
+                {item.first_name} {item.last_name}
+              </h2>
+              <p className="add" onClick={() => setTeam(team + 1)}>
+                Add to team
+              </p>
             </div>
-          </>
+          </div>
         );
       })}
     </div>
